597a31add4225cff441b9dc884decbe7f1d25dab
Christian Ponce <christianct2011@gmail.com>
2021-06-25 13:02:52 -0500

    Examen completando

--- Examen_Christian_Ponce/src/dominio/Plano.java
+++ Examen_Christian_Ponce/src/dominio/Plano.java
@@ -4,7 +4,7 @@ package dominio;
 
 public class Plano {
     private double x;
-  private double y;
+    private double y;
 
   // Constructor
   public Plano(double x, double y) {
@@ -36,44 +36,6 @@ public class Plano {
     double x1 = x - otroPunto.getX();
     double y1 = y - otroPunto.getY();
     return Math.sqrt(x1 * x1 + y1 * y1);
-  }
-  // devuelve el cuadrante
-  public int calcularCuadrante() {
-    if (x > 0.0 && y > 0.0) {
-      return 1;
-    } else if (x < 0.0 && y > 0.0) {
-      return 2;
-    } else if (x < 0.0 && y < 0.0) {
-      return 3;
-    } else if (x > 0.0 && y < 0.0) {
-      return 4;
-    } else {
-      // (x==0.0 || y==0.0)
-      return 0;
-    }
-  }
-
-  // calcula el punto mas cercano del array
-  public Plano calcularMasCercano(Plano[] otrosPuntos) {
-    Plano puntoMasCercano = null;
-
-    if (otrosPuntos != null && otrosPuntos.length > 0) {
-      // me guardo el primero como mas cercano
-      double distanciaMinima = calcularDistancia(otrosPuntos[0]);
-      puntoMasCercano = otrosPuntos[0];
-
-      for (int i = 1; i < otrosPuntos.length; i++) {
-        // si encuentro otro mas cercano, lo sustituyo
-        double distancia = calcularDistancia(otrosPuntos[i]);
-        if (distancia < distanciaMinima) {
-          distancia = distanciaMinima;
-          puntoMasCercano = otrosPuntos[i];
-        }
-      }
-    }
-    // devuelve el punto encontrado si el array contiene puntos, null en caso
-    // contrario
-    return puntoMasCercano;
   }
 }
 
--- Examen_Christian_Ponce/src/test/PruebaPlano.java
+++ Examen_Christian_Ponce/src/test/PruebaPlano.java
@@ -2,46 +2,28 @@
 package test;
 
 import dominio.Plano;
+import java.util.Scanner;
 
 
 public class PruebaPlano {
     public static void main(String args[]) {
-        
-    // Comprobar el numero de argumentos (el nombre de programa mas las dos
-    // coordenadas)
-    if (args.length != 2) {
-      System.out.println("Uso:");
-      System.out.println("  java Prueba x y");
-      !(4<5);
-      System.out.println("donde x e y son las cooordenadas de un punto en el espacio.");
-      return;
-    }
-    // Manejador de excepciones
-    try {
-      double x = Double.parseDouble(args[0]);
-      double y = Double.parseDouble(args[1]);
-
-      Plano p = new Plano(x, y);
-      System.out.println("El punto es " + p.toString());
-      System.out.println("Su distancia al origen es " + p.distanciaAlOrigen());
-      System.out.println("Su coordenada x es " + p.getX()+ " y su coordenada y es " + p.getY());
-      System.out.println("Esta ubicado en el cuadrante " + p.calcularCuadrante());
-
-      Plano[] arrayPuntos = new Plano[5];
-      arrayPuntos[0] = new Plano(1, 1);
-      arrayPuntos[1] = new Plano(5, 3);
-      arrayPuntos[2] = new Plano(10, 10);
-      arrayPuntos[3] = new Plano(-3, 2);
-      arrayPuntos[4] = new Plano(-4, -5);
-
-      for(int i = 0; i < arrayPuntos.length; i++) {
-        System.out.println("Su distancia al punto " + arrayPuntos[i].toString()
-            + " es " + p.calcularDistancia(arrayPuntos[i]));
-      }
-      System.out.println("El punto mas cercano es "
-          + p.calcularMasCercano(arrayPuntos));
-    } catch (NumberFormatException e) {
-      System.out.println("Error en los argumentos de la linea de comandos.");
-    }
+    double x,y;
+    Scanner entrada=new Scanner(System.in);
+    System.out.println("Digite el numero de movimientos a realizar: ");
+    int n=entrada.nextInt();    
+    x=(int)(Math.random()*100);
+    y=(int)(Math.random()*100);
+    
+  //  for(int i=0;i<n;i++){
+  //      for(int j=0;j<n;j++){
+            System.out.println(x+"\n");
+            System.out.println(y);
+            Plano p = new Plano(x, y);
+            System.out.println("El punto es: " + p.toString());
+            System.out.println("Su distancia es: " + p.distanciaAlOrigen());
+            System.out.println("Su coordenada x es " + p.getX()+ " y su coordenada y es " + p.getY());
+    //    }
+  //  }
+    
   }
 }
